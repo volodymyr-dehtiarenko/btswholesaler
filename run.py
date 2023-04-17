b579af6dcaa18bcb1a067293140617fef77196f6
volodymyr-dehtiarenko <volodymyr.dehtiarenko@gmail.com>
2023-04-17 21:44:13 +0200

    adding new def

--- run.py
+++ run.py
@@ -6,28 +6,24 @@ URL_SANDBOX  = 'https://apidev.btswholesaler.com/v1/api/getListProducts'
 TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInN1YiI6OTc2MzU2LCJpYXQiOjE2NzcxODM5MDZ9.CZk2L11F2LdqSO5Ww8ALKJjJcINQWpVN4t9Q4EjY-NA"
 
 
-
-class ApiBTS():
-    """ Connection to https://www.btswholesaler.com and download products"""
-
-    def api_conn():
-        head = {'Authorization': "Bearer {}".format(TOKEN)}
-        connect = requests.get(URL_PRODUCTS, headers=head)
-        if connect.status_code == 200:
-            print("Connection to BTS API - OK!", connect)
-            return connect
+def api_connect():
+    head = {'Authorization': "Bearer {}".format(TOKEN)}
+    _connect = requests.get(URL_PRODUCTS, headers=head)
+    if _connect.status_code == 200:
+        print("Connection to BTS API - OK!", _connect)
+    return _connect
 
 
-    def api_productCRUD():
-        for item in self.connect:
-                print(item)
+def api_productCRUD(**kwargs):
+    for item in api_connect():
+        # data = json.loads(item)
+        print(json.loads(item))
 
-    def item_adding():
-        pass
+
 
 if __name__ == '__main__':
-    ApiBTS.api_conn()
-    ApiBTS.api_productCRUD()
+    #api_connect()
+    api_productCRUD()
 
     
     
