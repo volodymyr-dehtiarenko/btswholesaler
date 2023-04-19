93d2f4831faba44ce7f929afcf70a9ea656d59df
volodymyr-dehtiarenko <volodymyr.dehtiarenko@gmail.com>
2023-04-19 21:03:04 +0200

    commit 19.04.2023

--- run.py
+++ run.py
@@ -1,47 +1,55 @@
-import json
-import requests
-
-URL_PRODUCTS = 'https://api.btswholesaler.com/v1/api/getListProducts'
-URL_SANDBOX  = 'https://apidev.btswholesaler.com/v1/api/getListProducts'
-TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInN1YiI6OTc2MzU2LCJpYXQiOjE2NzcxODM5MDZ9.CZk2L11F2LdqSO5Ww8ALKJjJcINQWpVN4t9Q4EjY-NA"
-
-
-def api_connect():
-    head = {'Authorization': "Bearer {}".format(TOKEN)}
-    _connect = requests.get(URL_PRODUCTS, headers=head)
-    if _connect.status_code == 200:
-        print("Connection to BTS API - OK!", _connect)
-    return _connect
-
-
-def api_productCRUD(**kwargs):
-    for item in api_connect():
-        # data = json.loads(item)
-        print(json.loads(item))
+from setting import TOKEN, URL_PRODUCTS
+import json, requests, pickle
+import simplejson
+from datetime import date
 
 
 
-if __name__ == '__main__':
-    #api_connect()
-    api_productCRUD()
+class ApiIntegrate:
+    """API Integration to database""" 
+
+    # def __init__(self, apidata):
+    #     self.apidata = apidata
+        
+    def api_connection():
+        head = {'Authorization': "Bearer {}".format(TOKEN)}
+        apidata = requests.get(URL_PRODUCTS, headers=head)
+        if apidata.status_code == 200:
+            print("Connection to BTS API - OK!")
+        return apidata
+    
+    def data_download():
+        today = date.today()
+        file_name = date.isoformat(today)
+        try:
+            with open('product_'+ file_name +'.json', "wb") as fp:
+                json.dump(apidata, fp)
+                print("File is ready - OK!")
+        except TypeError:
+            print("Sorry file not writening - NO!")
+        finally:
+            print("test")
+    
+    
+    def db_connection():
+        pass
 
     
     
+    def data_integration():
+        pass
 
-
-
-
-
-
-
-
-
-    # def run_api():
-    #     product_info = requests.get(url_product, headers=head)
-    #     if product_info.status_code == 200:
-    #         for prod in product_info:
-    #             print(prod)
     
+    
+    def data_update():
+        pass
+
+
+
+
+
+
+
 
 
 
---
+++ setting.py
@@ -0,0 +1,4 @@
+URL_PRODUCTS = 'https://api.btswholesaler.com/v1/api/getListProducts'
+URL_SANDBOX  = 'https://apidev.btswholesaler.com/v1/api/getListProducts'
+TOKEN        = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInN1YiI6OTc2MzU2LCJpYXQiOjE2NzcxODM5MDZ9.CZk2L11F2LdqSO5Ww8ALKJjJcINQWpVN4t9Q4EjY-NA"
+
